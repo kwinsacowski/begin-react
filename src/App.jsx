3004865226409a13c42726fad8f8060d3da169b9
Kayla Salmon <kayla.wienczkowski@yahoo.com>
2025-09-16 18:39:06 -0400

    basics of react understood

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from "./Header.jsx";
+import Message from "./Message.jsx"; 
+import Footer from "./Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>My First React App</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          I am not sure if I am ready.....
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Header />
+      <p>Welcome to your first React app!</p>
+    <div><Message /></div>
+    <div><Footer /></div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Message.jsx
@@ -0,0 +1,8 @@
+function Message() {
+    //jsx javascript xml
+    let name = "Kayla"
+    let tasks = 0;
+    return <h1>Hello {name}, you have {tasks} tasks</h1>;
+}
+
+export default Message;
